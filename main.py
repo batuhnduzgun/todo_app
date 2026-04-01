8cc44c125816a6bb030ac2d663f553601b48ceb4
Batuhan Düzgün <bduzgun28@gmail.com>
2026-04-01 12:59:45 +0300

    Store user inputs in a list using append and format with title

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 user_prompt = "Enter a todo:"
 
+todos = []
+
 while True:
     todo = input(user_prompt)
-    print(todo)
-    print("Next...")
+    print(todo.title())
+    todos.append(todo)
+    print(todos)
