2157c4e0c027cf81063c3d9cff10aeb852096ef0
Batuhan Düzgün <bduzgun28@gmail.com>
2026-04-01 15:05:17 +0300

    Add input stripping, for-loop display, and default case handling

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ todos = []
 
 while True:
     user_action = input("Type add, show or exit:")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo")
+            todo = input("Enter a todo:")
             todos.append(todo)
-        case 'show':
-            print(todos)
+        case 'show' | 'display':
+            for item in todos:
+                print(item.title())
         case 'exit':
             break
+        case _:
+            print("Hey you entered unknown command!")
 print("Bye!")
