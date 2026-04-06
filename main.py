88dddbfc6c7a34518084550aaea2a99d5f8c5194
Batuhan Düzgün <bduzgun28@gmail.com>
2026-04-06 15:36:00 +0300

    Add file reading capability to the show command

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ while True:
             file.writelines(todos)
             file.close()
         case 'show' | 'display':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item.title()}"
                 print(row)
